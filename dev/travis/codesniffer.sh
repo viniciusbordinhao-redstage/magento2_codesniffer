0d1c14438defcf8e18ea152a6208822d72415b60
Vinicius Bordinhão <vbordinhao@redstage.com>
2018-06-18 00:43:07 -0300

    test 3

--- dev/travis/codesniffer.sh
+++ dev/travis/codesniffer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-composer create-project --repository=https://repo.magento.com magento/marketplace-eqp magento-coding-standard
-~/magento-coding-standard/vendor/bin/phpcs --standard=MEQP2 --severity=10  --extensions=php,phtml ./app/code/
+git clone https://github.com/magento/marketplace-eqp.git ~/marketplace-eqp
+cd ~/marketplace-eqp && composer install
+pwd
+~/marketplace-eqp/vendor/bin/phpcs --standard=MEQP2 --severity=10  --extensions=php,phtml ~/magento2_clean/app/code/
 
 
 
