294854983a6ef38df9efa08150f8f7ad67750755
Vinicius Bordinhão <vbordinhao@redstage.com>
2018-06-18 00:26:55 -0300

    Adjusting codesniffer script

--- dev/travis/codesniffer.sh
+++ dev/travis/codesniffer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/magento/marketplace-eqp.git ~/marketplace-eqp
-cd ~/marketplace-eqp && composer install
+cd ~/marketplace-eqp && composer install && cd ~
 ~/marketplace-eqp/vendor/bin/phpcs --standard=MEQP2 --severity=10  --extensions=php,phtml ./app/code/
 
 
