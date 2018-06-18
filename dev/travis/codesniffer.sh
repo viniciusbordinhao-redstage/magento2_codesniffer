54c3bd78a71db70354cbb23a8a0c6b4cce069b02
Vinicius Bordinhão <vbordinhao@redstage.com>
2018-06-18 00:49:20 -0300

    test 4

--- dev/travis/codesniffer.sh
+++ dev/travis/codesniffer.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+path=${PWD}
 git clone https://github.com/magento/marketplace-eqp.git ~/marketplace-eqp
 cd ~/marketplace-eqp && composer install
-pwd
-~/marketplace-eqp/vendor/bin/phpcs --standard=MEQP2 --severity=10  --extensions=php,phtml ~/magento2_clean/app/code/
+cd $path
+~/marketplace-eqp/vendor/bin/phpcs --standard=MEQP2 --severity=10  --extensions=php,phtml ./app/code/
 
 
 
