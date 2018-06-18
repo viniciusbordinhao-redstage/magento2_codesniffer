3c048a486b46a90a00cb0a010a65f919badabfe1
Vinicius Bordinhão <vbordinhao@redstage.com>
2018-06-18 00:37:37 -0300

    Adjusting travis to use composer to install Magento coding standard

--- dev/travis/codesniffer.sh
+++ dev/travis/codesniffer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-git clone https://github.com/magento/marketplace-eqp.git ~/marketplace-eqp
-cd ~/marketplace-eqp && composer install && cd ~
-~/marketplace-eqp/vendor/bin/phpcs --standard=MEQP2 --severity=10  --extensions=php,phtml ./app/code/
+composer create-project --repository=https://repo.magento.com magento/marketplace-eqp magento-coding-standard
+~/magento-coding-standard/vendor/bin/phpcs --standard=MEQP2 --severity=10  --extensions=php,phtml ./app/code/
 
 
 
